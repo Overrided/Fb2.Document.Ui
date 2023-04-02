9326d87428a6d5f2b09895b4f0911a7987638c63
Denys Pukhkyi <den171093@gmail.com>
2023-04-02 17:39:26 +0300

    wpf playground initial commit

---
+++ Fb2.Document.WPF.Playground/Converters/Base64StringToImageConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Fb2.Document.WPF.Playground.Converters;
+
+public class Base64StringToImageConverter : IValueConverter
+{
+    private const string DefaultImageResourceNameString = "DefaultBookImage";
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var defaultBitmap = (BitmapImage)App.Current.Resources[DefaultImageResourceNameString];
+
+        if (value != null && !string.IsNullOrWhiteSpace(value.ToString()))
+        {
+            var base64ImageContent = value.ToString();
+            return ConvertBase64StringToBitmapImage(base64ImageContent) ?? defaultBitmap;
+        }
+
+        return defaultBitmap;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    private BitmapImage? ConvertBase64StringToBitmapImage(string? base64ImageContent)
+    {
+        if (string.IsNullOrEmpty(base64ImageContent))
+            return null;
+
+        try
+        {
+            var bitmap = new BitmapImage();
+
+            byte[] imageBytes = System.Convert.FromBase64String(base64ImageContent);
+            using var stream = new MemoryStream(imageBytes);
+
+            bitmap.BeginInit();
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.StreamSource = stream;
+            bitmap.EndInit();
+
+            if (bitmap.PixelHeight == 0 || bitmap.PixelWidth == 0)
+                return null;
+
+            return bitmap;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
---
+++ Fb2.Document.WPF.Playground/Models/BookModel.cs
@@ -0,0 +1,35 @@
+﻿namespace Fb2.Document.WPF.Playground.Models;
+
+public class BookModel
+{
+    public string FileName { get; set; } = string.Empty;
+    public string FilePath { get; set; } = string.Empty;
+    public long FileSizeInBytes { get; set; }
+    public string CoverPageBase64Image { get; set; } = string.Empty;
+    public string BookName { get; set; } = string.Empty;
+    public string BookAuthor { get; set; } = string.Empty;
+    public Fb2Document? Fb2Document { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is BookModel model &&
+               FileName == model.FileName &&
+               FilePath == model.FilePath &&
+               FileSizeInBytes == model.FileSizeInBytes &&
+               CoverPageBase64Image == model.CoverPageBase64Image &&
+               BookName == model.BookName &&
+               BookAuthor == model.BookAuthor &&
+               (Fb2Document == null && model.Fb2Document == null || (Fb2Document?.Equals(model.Fb2Document) ?? false));
+    }
+
+    public override int GetHashCode()
+    {
+        return (!string.IsNullOrEmpty(FileName) ? FileName.GetHashCode() : 0) ^
+               (!string.IsNullOrEmpty(FilePath) ? FilePath.GetHashCode() : 0) ^
+               FileSizeInBytes.GetHashCode() ^
+               (!string.IsNullOrEmpty(CoverPageBase64Image) ? CoverPageBase64Image.GetHashCode() : 0) ^
+               (!string.IsNullOrEmpty(BookName) ? BookName.GetHashCode() : 0) ^
+               (!string.IsNullOrEmpty(BookAuthor) ? BookAuthor.GetHashCode() : 0) ^
+               (Fb2Document != null ? Fb2Document.GetHashCode() : 0);
+    }
+}
