cc548e909c19b9aa7baaf8d903f22e1f83bdd920
Denys Pukhkyi <den171093@gmail.com>
2023-07-17 02:13:12 +0300

    code cleanup

--- MAUI/Fb2.Document.MAUI.Blazor.Playground/Data/BookModel.cs
+++ MAUI/Fb2.Document.MAUI.Blazor.Playground/Data/BookModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Fb2.Document.MAUI.Blazor.Playground.Data;
+﻿namespace Fb2.Document.MAUI.Blazor.Playground.Data;
 
 public class BookModel
 {
